672297d0423e2b11da9c362bf9fd2d7d7006321e
Oronno Akter Usha <ushaoronno1@gmail.com>
2024-02-21 23:56:16 +0600

    added more btn

--- script/script.js
+++ script/script.js
@@ -2,17 +2,26 @@ const allSeat = document.getElementsByClassName('seat');
 let count = 0;
 for(const seat of allSeat){
     seat.addEventListener("click", function (e) {
+        e.target.setAttribute('disabled', false);
+         const firstSeatCount = getConvertedValue('selected-seat');
+         if(firstSeatCount + 1 > 4){
+            alert('Please note: You can only select a maximum of four seats for this booking.');
+            return;
+         }
 
          const selectedBtn = seat;
          selectedBtn.style.backgroundColor = "#1DD100";
          selectedBtn.style.color = '#FFFFFF';
 
+        //  update seat
          const selectedSeat = getConvertedValue('selected-seat');
          document.getElementById('selected-seat').innerText = selectedSeat + 1;
 
          const seatLeft = getConvertedValue('seat-left');
          document.getElementById('seat-left').innerText = seatLeft - 1;
+
          
+        //  show the seat
          const seatInnerText = seat.innerText;
          const seatContainer = document.getElementById('show-seat');
          
@@ -50,6 +59,8 @@ function updateGrandTotal(status){
         if(couponCode == 'NEW15'){
             const discount = totalCost * .15;
             document.getElementById('grand-total').innerText = totalCost - discount;
+            const hideCoupon = document.getElementById('coupon-section');
+            hideCoupon.classList.add('hidden')
         }
         else if(couponCode == 'Couple 20'){
             const discount = totalCost * .2;
